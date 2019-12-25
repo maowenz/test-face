9dbeb3e4b4f2b33a0ade564ecd1898e3c7c4d75a
mwz <wenzheng.mao@zerosportsai.com>
2019-12-25 11:27:57 +0800

    compare

--- src/main/java/com/test/face/CompareFeature.java
+++ src/main/java/com/test/face/CompareFeature.java
@@ -24,14 +24,14 @@ public class CompareFeature {
 
 
         //String feature = "QpwAAAAAAAAMAgAAJXM3PbIivz0aMge9P3+qvQOqwr0p9Rs927QtPBOwMb0xM0m+0HPNPXf3hT1T/3s7D7STPWqINT1O1WS+kMs8vvlWPL16iwa+xFHJPfY92DtUgo898SQsu8r3Wr0yNFQ+fv3IvZpm3LxN67M9+KegOoIrwrzpebQ9jMwUPs2EJb0eW9S8dWDzvVOKBTy3YWo9OYvePZCitL02eRG8ofhwPPEdIjva+aE8LEwZvvoeF72Eq5u7p8wePvY+O77PJme84JfzPSIROL2V7Zq9BRqYvLsWxL31+O49ZnmcvTsTFj20obA94tf+u15lQb0lo3G9X+snvZrGVj2MCrm9U0xhvT6uAL40oUi9Gl7fvcqYob1kXRs9IY06PY7KXT2LnY49AToAvijA5z2USFk9cuOrO8UdND68RrY8zlPQvaGStD1TS+w9er4OPVCYtr0I0m2+9+Dbvf6xv70eQCQ9s6PFOz627T0Aita9BRGAPOlxybzTVy++kJ3mPW0txLtYsaS7p/bhvZbs7j3LfwY+oIIfvqv0F7yaR547RwT7PQZ/QrgGbz294i8Ovr4oNL0GoC09bdcEvZDmSL0JmGu9jNt5vBZj4j0Z1tc8ew8NPYDwBr65lds8UP5ZuxF1Nr26gKu9ZkkCPcNPVb18bNC95Q7oPCfEijyBdNc9pgCWPRML7D0=";
-        String feature = "QpwAAAAAAAAMAgAAI3ezun0Clr35SCG+clvKPeT3d72kKKq9LKkdvT9+ojuuwbu8KJuCvf8K570Ktu28YF8SPgeQEL0SXNm9ajdwvXayPDw18v89/nHHPDjeYD22ycS93bdSOjo6qLxkBHO9flv4PTPgWT5lrBM+4QesvbiP9rfPqnY7c8ERvZay3DzCKhC8DATiPWIlmD06qb08eNASPIKz0bx2Ac29o8NXvIDLnr11n4K8j2pbPEFzhb0fzAC+CKDEvbyx07wQVaQ9IkDfPO2/JLwMAQs++pFevSIfpD1qESI9GckVPdDgyTxvX+o6cnvivYsxcbyQUEU+iakIvZoeSryQGxW9JCLKvZzvGT5SCJ88/Wgfvi+mI7w+nD2+h+U4PSlzJr7nEVu9oC+TPVjOFr05dRI8sZziPfFWGD3nAO862B+oOySdozzTtOG8JjicvmK1VT0pDoe9R0omvFl9LL4mDLM7bGYUPik6DL747eg91lsZvnFNQzs5+eK9fco3vVHTvj1OP/O9AYxUvnUWuLsqYJI9xKqBPYd+6rxgGjo8eA1zvZpjqL3DVre8y1pJvWGMnb30KwI+fgR1vVkSKr7uuLg8FVm5vRZVxb26iIC8S+6UvfcH0Dy3z1y8vc4KvkyCKzyEW5y9XQtpvcI2RD74aW+9xfyLvZYfVT1JRIS9Q9SDPO4Boj0=";
+        //String feature = "QpwAAAAAAAAMAgAAI3ezun0Clr35SCG+clvKPeT3d72kKKq9LKkdvT9+ojuuwbu8KJuCvf8K570Ktu28YF8SPgeQEL0SXNm9ajdwvXayPDw18v89/nHHPDjeYD22ycS93bdSOjo6qLxkBHO9flv4PTPgWT5lrBM+4QesvbiP9rfPqnY7c8ERvZay3DzCKhC8DATiPWIlmD06qb08eNASPIKz0bx2Ac29o8NXvIDLnr11n4K8j2pbPEFzhb0fzAC+CKDEvbyx07wQVaQ9IkDfPO2/JLwMAQs++pFevSIfpD1qESI9GckVPdDgyTxvX+o6cnvivYsxcbyQUEU+iakIvZoeSryQGxW9JCLKvZzvGT5SCJ88/Wgfvi+mI7w+nD2+h+U4PSlzJr7nEVu9oC+TPVjOFr05dRI8sZziPfFWGD3nAO862B+oOySdozzTtOG8JjicvmK1VT0pDoe9R0omvFl9LL4mDLM7bGYUPik6DL747eg91lsZvnFNQzs5+eK9fco3vVHTvj1OP/O9AYxUvnUWuLsqYJI9xKqBPYd+6rxgGjo8eA1zvZpjqL3DVre8y1pJvWGMnb30KwI+fgR1vVkSKr7uuLg8FVm5vRZVxb26iIC8S+6UvfcH0Dy3z1y8vc4KvkyCKzyEW5y9XQtpvcI2RD74aW+9xfyLvZYfVT1JRIS9Q9SDPO4Boj0=";
 
 
         try {
             StFaceVerify stFaceVerify = new StFaceVerify(alignModel, verifyModel);
             StFaceFeature feature1 = GetFeature.testFace(args[0]);
-            //StFaceFeature feature2 = GetFeature.testFace(args[1]);
-            StFaceFeature feature2 = StFaceFeature.createStFaceFeature(feature.getBytes());
+            StFaceFeature feature2 = GetFeature.testFace(args[1]);
+            //StFaceFeature feature2 = StFaceFeature.createStFaceFeature(feature.getBytes());
             if(null != feature1) {
                 float score = stFaceVerify.compareFeature(feature1, feature2);
                 System.out.println("compare score: " + score);

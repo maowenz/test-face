6e30d064ea441852969116d709613617018b2971
mwz <wenzheng.mao@zerosportsai.com>
2020-08-13 10:47:58 +0800

    log

--- src/main/java/com/test/face/BodyTest.java
+++ src/main/java/com/test/face/BodyTest.java
@@ -10,6 +10,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.logging.Logger;
 
 /**
  * @author wzm
@@ -43,18 +44,24 @@ public class BodyTest {
         int retcode[] = new int[1];
         String bodydetect = "/home/data/body/models/M_Detect_Body_Hunter_1.7.0.model";
         String bodyaligment = "/home/data/body/models/M_Detect_Body_Keypoints_5.5.0.model";
-        String testImage = "./4.jpeg";
+        String testImage = "./4.png";
         //LibraryUtil.addLibraryPath("./system/");
         String license = readLicense("/home/data/sense-face/sense-time-license.lic");
         String signedCode = StLibrary.generateActivationCode("SenseInsight", license,retcode);
         retcode[0] = StLibrary.checkActivationCode("SenseInsight", license, signedCode);
 
         StLibrary.stEnvInit(license, signedCode, "SenseInsight", "/home/data/body/senu.kep", "/home/data/body/classifier.kep", retcode);
+        System.out.println("环境初始化，result：" + retcode[0]);
+
         long classifier = StLibrary.stClassifierCreate("{\"model\":\"/home/data/body/models/KM_Classifier_Ped_Filter_ppl_1.2.1.model\"}",retcode);
-        //Assert.assertTrue("Create env failed! errorcode  = " + retcode[0], retcode[0] == 0);
+        System.out.println("创建人体质量句柄，result：" + retcode[0]);
+
         long reidHandle = StLibrary.stReIdInitHandle("{\"model\":\"/home/data/body/models/KM_Senu_ppl_1.4.5.model\"}",retcode);
-//        Assert.assertTrue("Create reidHandle failed! errorcode  = " + retcode[0], reidHandle != 0);
+        System.out.println("创建特征提取句柄，result：" + retcode[0]);
+
         long bodyHandler = StLibrary.stCommonBodyTrackingCompactCreate(bodydetect, bodyaligment, retcode);
+        System.out.println("创建人体检测句柄，result：" + retcode[0]);
+
         int width[] = new int[1];
         int height[] = new int[1];
         long cur2 = System.currentTimeMillis();
@@ -69,12 +76,13 @@ public class BodyTest {
             }
         }while (bodies == null || bodies.length == 0);
         float quality = StLibrary.stGetQuality(classifier, image, 5, width[0], height[0], bodies[0], retcode);
+        System.out.println("人体质量："+quality);
         long cur = System.currentTimeMillis();
         byte feature1[] = StLibrary.stReIdProcessImage(reidHandle, image, 5, width[0], height[0], bodies[0], retcode);
         System.out.println("zqt time = " + (System.currentTimeMillis() - cur));
         System.out.println("zqt time1 = " + (System.currentTimeMillis() - cur2));
 
-        image = StLibrary.stGetBgrDataFromImage("./1.jpg", width, height);
+        image = StLibrary.stGetBgrDataFromImage("./1.png", width, height);
         do{
             bodies = StLibrary.stCommonBodyTrackingCompactTrack(bodyHandler, image, 5, width[0], height[0], 1, retcode);
             try {
